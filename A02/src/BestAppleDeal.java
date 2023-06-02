d1920829ba51d18aee289a1db0e561cd1f312214
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-02 14:53:35 -0400

    A02P3-init

---
+++ A02/src/BestAppleDeal.java
@@ -0,0 +1,32 @@
+// Class:       CS 5000 / W01
+// Term:        Summer 2023
+// Name:        Matthew Gibson
+// Instructor:  Dr. Haddad
+// Assignment:  2
+// IDE Name:    VS Code
+
+import java.util.Scanner;
+
+public class BestAppleDeal {
+  public static void main(String[] args) {
+    // setup/input section
+    // wrapper to optionally skip prompt, if cmdline parameter is entered.
+    int inputValue;
+    if (args.length == 0) {
+      Scanner s = new Scanner(System.in);
+      System.out.println();
+      System.out.print("Please enter the small box weight in pounds: ");
+      inputValue = s.nextInt();
+      s.close(); // prompted in VS Code to close to free up resources
+    } else {
+      inputValue = Integer.parseInt(args[0]);
+    }
+    // processing section
+
+    // output section
+    System.out.println();
+    System.out.println("Input Value:\t" + inputValue);
+
+    // cleanup section, any resources to close?
+  }
+}
