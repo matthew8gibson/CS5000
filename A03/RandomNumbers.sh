c76ac2bba7eeda167b0155f71639850bbad75a26
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-03 15:56:18 -0400

    update

---
+++ A03/RandomNumbers.sh
@@ -0,0 +1,3 @@
+for i in {1..5}; do echo "Hello World"; done
+
+for i in {1..200}; do cd /Users/matgib/repos/CS5000/A03 ; /usr/bin/env /Library/Java/JavaVirtualMachines/temurin-20.jdk/Contents/Home/bin/java --enable-preview -XX:+ShowCodeDetailsInExceptionMessages -cp /Users/matgib/repos/CS5000/A03/bin RandomNumbers; done
--- A03/src/RandomNumbers.java
+++ A03/src/RandomNumbers.java
@@ -14,16 +14,34 @@ public class RandomNumbers {
 
     // processing section
     // a) A random integer number between 20 and 80 (inclusive).
-    requirement1 = 20 + (int) (Math.random() * 60);
+    requirement1 = 20 + (int) (Math.random() * 61);
     // b) A random integer number between -20 and 20 (inclusive).
-
+    requirement2 = -20 + (int) (Math.random() * 41);
     // c) A random integer number between -50 and -20 (inclusive).
-
+    requirement3 = -50 + (int) (Math.random() * 31);
     // d) A random floating-point number between 0.0 and 21.9999 (inclusive).
+    requirement4 = (float) ((Math.random() * 21.999f));
+    // TODO: broken? 21.99 won't ever be hit
+
+    boolean showDetails = true;
+    if (showDetails) {
+      if (requirement1 == 20 || requirement1 == 80 ||
+          requirement2 == -20 || requirement2 == 20 ||
+          requirement3 == -50 || requirement3 == -20 ||
+          requirement4 == 0.0f || requirement4 == 21.999f) {
+        System.out.println();
+        System.out.println("testing**************" + requirement1 + " "
+            + requirement2 + " " + requirement3 + " " + requirement4);
+        System.out.println();
+      }
+    }
 
     // output section
-    System.out.println();
+    // System.out.println();
     System.out.println("a) A random integer between 20 and 80 (inclusive):\t" + requirement1);
+    System.out.println("b) A random integer number between -20 and 20 (inclusive):\t" + requirement2);
+    System.out.println("c) A random integer number between -50 and -20 (inclusive):\t" + requirement3);
+    System.out.println("d) A random floating-point number between 0.0 and 21.9999 (inclusive);\t" + requirement4);
 
     // cleanup section, any resources to close?
   }
