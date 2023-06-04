091d63fa5ae1a238e31017851e5702d35a686622
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-03 20:58:02 -0400

    A03P2-fixed bug related 21.9999 inclusive!

--- A03/src/RandomNumbers.java
+++ A03/src/RandomNumbers.java
@@ -10,31 +10,44 @@ public class RandomNumbers {
     // setup/input section
     // wrapper to optionally skip prompt, if cmdline parameter is entered.
     int requirement1, requirement2, requirement3;
-    float requirement4;
+    float requirement4 = 0;
 
     // processing section
     // a) A random integer number between 20 and 80 (inclusive).
-    requirement1 = 20 + (int) (Math.random() * 61);
+    requirement1 = 33333;
     // b) A random integer number between -20 and 20 (inclusive).
     requirement2 = -20 + (int) (Math.random() * 41);
     // c) A random integer number between -50 and -20 (inclusive).
     requirement3 = -50 + (int) (Math.random() * 31);
     // d) A random floating-point number between 0.0 and 21.9999 (inclusive).
-    requirement4 = (float) ((Math.random() * 21.999f)); // BUG: 21.99 will not get hit!
-    // IDEA: can we using casting of double to float to get it to be inclusive?
+    double dblReq4 = ((Math.random() * 22)); // this functions uses double
+    String strReq4 = String.format("%.4f", dblReq4); // do this to get to 4 decimals
+    requirement4 = Float.parseFloat(strReq4); // moving back to the format needed
 
     // debugging section, disable by setting to false
-    boolean showDetails = false;
+    boolean showDetails = true;
     if (showDetails) {
-      // if (requirement2 == -20 || requirement2 == 20){
-      // if (requirement3 == -50 || requirement3 == -20){
-      if (requirement4 == 0.0f || requirement4 == 21.999f) {
-        // if (requirement1 == 20 || requirement1 == 80) {
+      int count = 0;
+      while (showDetails) {
+        ++count;
+        // BEGIN TEST SECTION
+        requirement1 = 20 + (int) (Math.random() * 61);
+        // END TEST SECTION
 
-        System.out.println();
-        System.out.println("testing**************" + requirement1 + " "
-            + requirement2 + " " + requirement3 + " " + requirement4);
-        System.out.println();
+        // comment and uncomment these lines to test.
+        // if (requirement2 == -20 || requirement2 == 20) {
+        // if (requirement3 == -50 || requirement3 == -20) {
+        // if (requirement4 == 0.0f || requirement4 == 21.999f) {
+        // if (requirement4 == 21.9999f) {
+        if (requirement1 == 20 || requirement1 == 80) {
+
+          System.out.println();
+          System.out.println("testing**************" + requirement1 + " "
+              + requirement2 + " " + requirement3 + " " + requirement4);
+          System.out.println(count);
+          System.out.println(requirement1);
+          showDetails = false; // found it!
+        }
       }
     }
 
