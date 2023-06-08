5e616c428526b92025de4a9b008f6e8eaac6426b
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-08 17:06:56 -0400

    A04-P3-init

---
+++ A04/src/Pattern.java
@@ -0,0 +1,29 @@
+// Class:       CS 5000 / W01
+// Term:        Summer 2023
+// Name:        Matthew Gibson
+// Instructor:  Dr. Haddad
+// Assignment:  2
+// IDE Name:    VS Code
+
+import java.util.Scanner;
+
+public class Pattern {
+  public static void main(String[] args) {
+    // setup/input section
+    int rows = 6;
+    int columns = 6;
+
+    // processing section
+    for (int i = 1; i < rows; i++) {
+      for (int j = 1; j < columns; j++) {
+
+      }
+
+    }
+
+    // output section
+    System.out.println();
+
+    // cleanup section, any resources to close?
+  }
+}
