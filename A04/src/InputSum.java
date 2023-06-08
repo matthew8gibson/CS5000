13c4a10dd48117bba62f8f475d6d008f07811e9b
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-08 11:38:43 -0400

    A04-P1-complete

--- A04/src/InputSum.java
+++ A04/src/InputSum.java
@@ -14,32 +14,34 @@ public class InputSum {
 
     {
       // setup section
-      int sum = 0, value, count = 0;
+      int sum = 0, value;
+      String allEnteredNumbers = "";
       Scanner scan = new Scanner(System.in);
 
-      // Processing and taking input
+      // Processing and taking input section
       System.out.print("Enter positive integers (-1 to quit): ");
-      value = scan.nextInt();
-      while (value != -1)
-      // sentinel value of -1 to terminate loop
-      {
-        if (value > 0) { // not counting negative numbers
+      do {
 
+        value = scan.nextInt();
+        if (value > 0) { // not counting negative numbers
+          if (allEnteredNumbers != "") {
+            // not the first time, so include the comma
+            allEnteredNumbers = allEnteredNumbers.concat(", ");
+          }
+          // only adding positive numbers
+          allEnteredNumbers = allEnteredNumbers.concat(String.valueOf(value));
           sum = sum + value;
+
         } else {
-          System.out.println("The sum so far is " + sum);
-          System.out.print("Enter positive integers (-1 to quit): ");
-          value = scan.nextInt();
+          // negative numbers hit here. they are not added to the string or sum
         }
-      }
+        // System.out.println("Debugging: The sum so far is " + sum);
+      } while (value != -1); // end the loop when sentinel value is entered
 
       // output section
       System.out.println();
-      if (count == 0) {
-        System.out.println("No values were entered.");
-      } else {
-        System.out.println("Sum of all values = " + sum + "\n" + "Average of all values = " + (sum / (double) count));
-      }
+      System.out.println("Entered numbers:\t" + allEnteredNumbers);
+      System.out.println("The Sum:\t\t" + sum);
 
       // cleanup
       scan.close(); // cleaning up resources
