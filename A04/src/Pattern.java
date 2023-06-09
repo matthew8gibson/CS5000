708da11aa81d78902155b4a5e8f860a8c250edf4
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-09 09:30:56 -0400

    A04-P3-complete

--- A04/src/Pattern.java
+++ A04/src/Pattern.java
@@ -2,28 +2,33 @@
 // Term:        Summer 2023
 // Name:        Matthew Gibson
 // Instructor:  Dr. Haddad
-// Assignment:  2
+// Assignment:  4
 // IDE Name:    VS Code
-
-import java.util.Scanner;
 
 public class Pattern {
   public static void main(String[] args) {
     // setup/input section
     int rows = 6;
-    int columns = 6;
-
-    // processing section
-    for (int i = 1; i < rows; i++) {
-      for (int j = 1; j < columns; j++) {
-
-      }
-
-    }
 
     // output section
+    // space at the beginning
     System.out.println();
 
-    // cleanup section, any resources to close?
+    for (int i = rows; i >= 1; i--) { // counting down
+
+      // spaces to move number to the right
+      for (int j = 1; j <= rows - i; j++) { // counting up
+        System.out.print(" ");
+      }
+
+      // numbers
+      for (int k = 1; k <= i; k++) {
+        System.out.print(k);
+      }
+
+      System.out.println(); // move to the next line
+    }
+
+    // cleanup section, any resources to close? -- nope!
   }
 }
