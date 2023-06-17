7204aa49eb2fa133904e7613790face7e00aab85
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-17 09:24:48 -0400

    midterm-practice

--- MidTermPractice1/src/stringfun.java
+++ MidTermPractice1/src/stringfun.java
@@ -1,18 +1,24 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class stringfun {
-    Scanner scan = new Scanner(System.in)
-    //do some string fun
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    // do some string fun
     System.out.print("Hello. Please enter your first and last name: ");
+
+    String lastName;
     String firstAndLastName = scan.nextLine();
     System.out.print("Hello. Please enter your city: ");
     String city = scan.nextLine();
 
-    System.out.println("Length: " firstAndLastName.Length());
+    System.out.println("Length: " + firstAndLastName.length());
     System.out.println("You are " + firstAndLastName + " from " + city);
     System.out.println(firstAndLastName.concat(" of ").concat(city));
-    System.out.println(firstAndLastName.toUppercase());
-    System.out.println(firstAndLastName.toLowercase());
-    lastName = firstAndLastName.substring((firstAndLastName.indexof(" ") + 1), (firstAndLastName.Length() - 1));
+    System.out.println(firstAndLastName.toUpperCase());
+    System.out.println(firstAndLastName.toLowerCase());
+    lastName = firstAndLastName.substring((firstAndLastName.indexOf(" ") + 1), (firstAndLastName.length() - 1));
     System.out.println(lastName.concat(" is your last name"));
+    scan.close();
+  }
 }
