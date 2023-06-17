29490a45554c4e74dabc8850e93895e859675467
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-17 08:46:17 -0400

    study

---
+++ MidTermPractice1/src/PlayGround1.java
@@ -0,0 +1,14 @@
+public class PlayGround1 {
+  public static int x = 5;
+
+  public static void myMethod() {
+    int x = 10;
+    System.out.println(x);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(x);
+    myMethod();
+  }
+
+}
