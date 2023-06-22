54c5e8732cd499e0b29bcce89aa557b10d589492
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-22 17:03:24 -0400

    study

--- MidTermPractice1/src/Loop1.java
+++ MidTermPractice1/src/Loop1.java
@@ -1,9 +1,11 @@
 public class Loop1 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; ++i) {
             System.out.println(i);
         }
-
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(i); // more common solution
+        }
     }
 }
