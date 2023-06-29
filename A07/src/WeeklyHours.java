7f118a97e26422b4c509ee8c36f2162744ad463b
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-06-29 10:09:18 -0400

    added sentinel loop

--- A07/src/WeeklyHours.java
+++ A07/src/WeeklyHours.java
@@ -5,36 +5,55 @@
 // Assignment:  7
 // IDE Name:    VS Code
 
+import java.util.Scanner;
+
 public class WeeklyHours {
 
   public static void main(String[] args) {
 
+    // sentinel setup
+    Scanner scan = new Scanner(System.in);
+    String sentinelValue = "Y";
+    String sentinelResponse = "Y";
+
+    // main setup
     int numberOfEmployees = 3;
     int daysOfTheWeek = 7;
     int[][] arrayHoursWorked = new int[numberOfEmployees][daysOfTheWeek];
 
-    // populates the array with random numbers between 0 and 10
-    for (int i = 0; i < numberOfEmployees; i++) {
-      for (int j = 0; j < daysOfTheWeek; j++) {
-        arrayHoursWorked[i][j] = GenerateDailyHours();
+    while (sentinelResponse.equals(sentinelValue)) {
+      // populates the array with daily hours
+      for (int i = 0; i < numberOfEmployees; i++) {
+        for (int j = 0; j < daysOfTheWeek; j++) {
+          arrayHoursWorked[i][j] = GenerateDailyHours();
+        }
       }
-    }
 
-    // print the table header
-    System.out.println();
-    System.out.println("\t\tMon\tTue\tWed\tThu\tFri\tSat\tSun");
+      // print the table header
+      System.out.println();
+      System.out.println("\t\tMon\tTue\tWed\tThu\tFri\tSat\tSun");
 
-    // print the table body
-    for (int i = 0; i < numberOfEmployees; i++) {
-      System.out.printf("Employee%d\t ", i + 1); // first column
-      for (int j = 0; j < daysOfTheWeek; j++) {
-        System.out.print(arrayHoursWorked[i][j] + "\t "); // other columns
+      // print the table body
+      for (int i = 0; i < numberOfEmployees; i++) {
+        System.out.printf("Employee%d\t ", i + 1); // first column
+        for (int j = 0; j < daysOfTheWeek; j++) {
+          System.out.print(arrayHoursWorked[i][j] + "\t "); // other columns
+        }
+        System.out.println(); // space after the table to make it less cluttered
       }
-      System.out.println(); // space after the table to make it less cluttered
-    }
 
-    // call function to summarize hours per employee
-    addHours(arrayHoursWorked);
+      // call function to summarize hours per employee
+      addHours(arrayHoursWorked);
+
+      // sentinel input section
+      System.out.println();
+      // scan.nextLine(); // clear the thing caused by .nextInt()
+      System.out.print("Do you want to do it again? (Y to continue): ");
+      sentinelResponse = scan.nextLine().toUpperCase().trim();
+    } // end of sentinel loop
+
+    // cleanup
+    scan.close();
 
   }
 
