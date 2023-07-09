f0078773f5da13c05e8d9f702c19387b84580326
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-07-09 14:34:18 -0400

    A08P1-done

--- A08/src/TestAccount.java
+++ A08/src/TestAccount.java
@@ -12,41 +12,36 @@ import java.util.Scanner;
 public class TestAccount {
   public static void main(String[] args) {
 
-    // part 1 set values
-    Account myObject = new Account(123456, 10000);
-    myObject.setAnnualInterestRate(.025);
-    myObject.withdraw(3500);
-    myObject.deposit(500);
-    // part 1 print values
+    // setup
     System.out.println();
-    System.out.println("First Test Output:");
-    System.out.println(myObject);
-
-    // part 3 - allowing user input and via sentinel loop.
-    System.out.println("Now, for the interactive part:");
-    System.out.println();
-
-    // As for User Input:
     Scanner scan = new Scanner(System.in);
+    double annualInterestRate;
 
-    // annualInterestRate is not in sentinel loop...
-    // since all users will have the same interest rate:
+    do {
+      System.out.println("The interest rate will be the same for all accounts.");
+      System.out.print("What is the annual interest rate? (e.g. 3.5 )\t");
+      annualInterestRate = scan.nextDouble() / 100.0; // conversion
+      scan.nextLine(); // clear the line return
+      if (!CheckAnnualInterestRate(annualInterestRate)) {
+        System.out.println("Annual interest rate must be between 0 and 100.");
+      }
+    } while (!CheckAnnualInterestRate(annualInterestRate));
+    System.out.println();
 
     // setup sentinel
     String sentinelValue = "Y";
     String sentinelResponse = "Y";
-
+    // start sentinel
     while (sentinelResponse.equals(sentinelValue)) {
 
+      // vars for inside the loop.
       int id;
       double balance, deposit, withdrawal;
-      double annualInterestRate;
 
-      // BEGIN the loop of one account
-      System.out.println("Please enter the relevant details for a new account #1:");
+      System.out.println("Please enter the relevant details for the account:");
       System.out.println();
 
-      // no checking or conditions on ID?
+      // no checking or conditions on ID
       System.out.print("What will the ID be?\t\t\t\t");
       id = scan.nextInt();
       scan.nextLine(); // clear the line return
@@ -60,15 +55,6 @@ public class TestAccount {
         }
 
       } while (!CheckPositive(balance));
-
-      do {
-        System.out.print("What is the annual interest rate? (e.g. 3.5 )\t");
-        annualInterestRate = scan.nextDouble() / 100.0; // conversion
-        scan.nextLine(); // clear the line return
-        if (!CheckAnnualInterestRate(annualInterestRate)) {
-          System.out.println("Annual interest rate must be between 0 and 100.");
-        }
-      } while (!CheckAnnualInterestRate(annualInterestRate));
 
       Account myAccount = new Account(id, balance);
       myAccount.setAnnualInterestRate(annualInterestRate);
@@ -85,7 +71,7 @@ public class TestAccount {
 
       Boolean withdrawalResult;
       do {
-        System.out.print("how much would you like to withdraw?\t\t");
+        System.out.print("How much would you like to withdraw?\t\t");
         withdrawal = scan.nextDouble();
         scan.nextLine(); // clear the line return
         withdrawalResult = myAccount.withdraw(withdrawal);
@@ -97,71 +83,11 @@ public class TestAccount {
       // show the summary
       System.out.println(myAccount);
 
-      // END the loop of one account - myAccount
-
-      // ** add the other account.
-
-      // BEGIN the loop of second account
-      System.out.println("Please enter the relevant details for a new account #2:");
-      System.out.println();
-
-      // no checking or conditions on ID?
-      System.out.print("What will the ID be?\t\t\t\t");
-      id = scan.nextInt();
-      scan.nextLine(); // clear the line return
-
-      do {
-        System.out.print("What is the balance?\t\t\t\t");
-        balance = scan.nextDouble();
-        scan.nextLine(); // clear the line return
-        if (!CheckPositive(balance)) {
-          System.out.println("The balance cannot be negative.");
-        }
-
-      } while (!CheckPositive(balance));
-
-      do {
-        System.out.print("What is the annual interest rate? (e.g. 3.5 )\t");
-        annualInterestRate = scan.nextDouble() / 100.0; // conversion
-        scan.nextLine(); // clear the line return
-        if (!CheckAnnualInterestRate(annualInterestRate)) {
-          System.out.println("Annual interest rate must be between 0 and 100.");
-        }
-      } while (!CheckAnnualInterestRate(annualInterestRate));
-
-      Account yourAccount = new Account(id, balance);
-      yourAccount.setAnnualInterestRate(annualInterestRate);
-
-      do {
-        System.out.print("how much would you like to deposit?\t\t");
-        deposit = scan.nextDouble();
-        scan.nextLine(); // clear the line return
-        if (!CheckPositive(deposit)) {
-          System.out.println("the value must be positive.");
-        }
-      } while (!CheckPositive(deposit));
-      yourAccount.deposit(deposit);
-
-      do {
-        System.out.print("how much would you like to withdraw?\t\t");
-        withdrawal = scan.nextDouble();
-        scan.nextLine(); // clear the line return
-        withdrawalResult = yourAccount.withdraw(withdrawal);
-        if (!withdrawalResult) {
-          System.out.println("You don't have that much money.");
-        }
-      } while (!withdrawalResult);
-
-      // show the summary
-      System.out.println(yourAccount);
-
-      // END the loop of second account - yourAccount
-
       // sentinel checking
       System.out.print("Do you want to do it again? (Y to continue): ");
       sentinelResponse = scan.nextLine().toUpperCase().trim();
       System.out.println();
-    }
+    } // end sentinel loop
 
     // cleanup.
     scan.close();
