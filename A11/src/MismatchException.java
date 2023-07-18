3ea85e60f5c533f42eeca540654186fbf6cef8a4
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-07-18 19:10:25 -0400

    A11-created

---
+++ A11/src/MismatchException.java
@@ -0,0 +1,18 @@
+// Class: CS 5000 / W01
+// Term: Summer 2023
+// Name: Matthew Gibson
+// Instructor: Dr. Haddad
+// Assignment: 11
+// IDE Name: VS Code
+
+import java.util.Scanner;
+
+@SuppressWarnings("unused")
+
+public class MismatchException {
+
+      public static void main(String[] args) {
+            System.out.println("Hello You!");
+      }
+
+}
