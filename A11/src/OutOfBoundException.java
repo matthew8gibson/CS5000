fdf58edf21dda36cea3d92caabb37107a35ea449
matthew8gibson <39424233+matthew8gibson@users.noreply.github.com>
2023-07-21 06:21:17 -0400

    save

--- A11/src/OutOfBoundException.java
+++ A11/src/OutOfBoundException.java
@@ -19,38 +19,61 @@ public class OutOfBoundException {
         int maxValueInArray = 100;
         int value = -1;
 
-        // populate my array
-        int[] myArray = new int[arraySize];
-        for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = (int) (Math.random() * (maxValueInArray + 1));
-            // System.out.println(myArray[i]);
-        }
+        // setup sentinel
+        // Scanner scan = new Scanner(System.in);
+        String sentinelValue = "Y";
+        String sentinelResponse = "Y";
 
-        System.out.print("Enter valid index between 0 and 9: ");
+        while (sentinelResponse.equals(sentinelValue)) {
 
-        // this while loop requires no exception to be raised on both entering the data
-        // and checking the array index location
-        while (!isValidInput) {
-            // problems to catch with user data entry:
-            // 1. user enters something which is not an integer
-            // 2. user enters an integer is which not between (zero and (arraySize - 1))
-
-            // collecting user data
-            try {
-                input = scan.nextInt(); // could generate InputMismatchException (problem 1)
-                value = myArray[input]; // could generate IndexOutOfBoundsException (problem 2)
-                // this code won't run, if an error is thrown!
-                isValidInput = true;
-            } catch (InputMismatchException | IndexOutOfBoundsException e) {
-                // one of the 2 problems got caught here
-                scan.nextLine(); // clear buffer
-                System.out.print("Index out of bound, re-enter a valid index: ");
+            // section of code which will be optionally run multiple times - BEGIN
+            // populate my array
+            int[] myArray = new int[arraySize];
+            for (int i = 0; i < myArray.length; i++) {
+                myArray[i] = (int) (Math.random() * (maxValueInArray + 1));
+                // System.out.println(myArray[i]);
             }
-            scan.nextLine(); // clear buffer (this is not needed in this location, but seems like a good idea
-                             // to do it incase more prompts are added later)
-        }
+            System.out.println("Debug: " + scan.hasNext() + " line36");
 
-        System.out.printf("The element at index %d is %d\n", input, value);
+            System.out.print("Enter valid index between 0 and 9: ");
+
+            // this while loop requires an exception to be raised on both entering the data
+            // and checking the array index location
+            while (!isValidInput) {
+                // problems to catch with user data entry:
+                // 1. user enters something which is not an integer
+                // 2. user enters an integer is which not between (zero and (arraySize - 1))
+
+                // collecting user data
+                try {
+                    input = scan.nextInt(); // could generate InputMismatchException (problem 1)
+                    value = myArray[input]; // could generate IndexOutOfBoundsException (problem 2)
+                    // this code won't run, if an error is thrown!
+                    // so if it runs, the input is valid
+                    isValidInput = true;
+                } catch (InputMismatchException ime) {
+                    // one of the 2 problems got caught here
+                    scan.nextLine(); // clear buffer
+                    System.out.print("Index out of bound, re-enter a valid index: ");
+                } catch (IndexOutOfBoundsException ioobe) {
+                    // one of the 2 problems got caught here
+                    scan.nextLine(); // clear buffer
+                    System.out.print("Index out of bound, re-enter a valid index: ");
+
+                }
+                scan.nextLine(); // clear buffer (this is not needed in this location, but seems like a good idea
+                                 // to do it incase more prompts are added later)
+            }
+
+            System.out.printf("The element at index %d is %d\n", input, value);
+
+            // section of code which will be optionally run multiple times - END
+
+            // sentinel checking
+            System.out.print("Would you like to run program again (y/n)? ");
+            sentinelResponse = scan.nextLine().toUpperCase().trim();
+
+        }
 
         // cleanup
         scan.close();
